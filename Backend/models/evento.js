0d67075cbbf2085b4ced993dbcda3cfd4d54400a
Bauti Garaventa <bautistagaraventa@hotmail.com>
2017-11-13 18:24:53 -0300

    agregado de atributo en un model

--- Backend/models/evento.js
+++ Backend/models/evento.js
@@ -2,6 +2,7 @@ var mongoose =require('mongoose');
 
 var eventoSchema= new mongoose.Schema({
   fecha_hora: {type: Date, required: true},
+  descripcion: {type: String},
   tipo_evento: {type: mongoose.Schema.Types.ObjectId, ref: 'tipo_evento'},
   partido: {type: mongoose.Schema.Types.ObjectId, ref: 'partido'},
   equipo: {type: mongoose.Schema.Types.ObjectId, ref: 'equipo'},
